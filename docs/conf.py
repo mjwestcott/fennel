313d9b44748ef23f0030c9bef35c64cc2bf606d3
Matt Westcott <m.westcott@gmail.com>
2020-08-07 11:14:21 +0100

    Change sys.path for Sphinx autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,12 @@
 import os
 import sys
 
-from sphinx.ext.autodoc import ClassLevelDocumenter, InstanceAttributeDocumenter
+from sphinx.ext.autodoc import (
+    ClassLevelDocumenter,
+    InstanceAttributeDocumenter,
+)
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # XXX: Monkey-patch to remove `=None` from attribute annotations.
